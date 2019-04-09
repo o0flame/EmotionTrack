7a68dea27d1ce74a55337280d2683397ca6e6aa0
Xiao, Feng <fxiao9@gatech.edu>
2019-04-09 16:46:47 -0400

    merge

--- app/src/main/java/com/example/emotiontrack/DashBoard.java
+++ app/src/main/java/com/example/emotiontrack/DashBoard.java
@@ -6,9 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Calendar;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -44,13 +49,36 @@ public class DashBoard extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        Date currentTime = Calendar.getInstance().getTime();
+        Calendar calendar = Calendar.getInstance();
+        //start from Sunday = 1;
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
 
         Intent intent = getIntent();
         Map<String,String> user_info = (HashMap<String,String>) intent.getSerializableExtra("user_info");
-        System.out.println(intent.getSerializableExtra("user_info").toString());
-        System.out.println("received intent, map size: " + user_info.size());
+
+        String[] res = user_info.get("emotion").split(",");
+        if(res.length!=7) System.out.println("emotion size is wrong");
+
+        int[] emotions = new int[res.length];
+        for(int i=0;i<emotions.length;i++) emotions[i] = Integer.parseInt(res[i]);
+
+
         welcomeText = findViewById(R.id.welcome);
         welcomeText.setText("Hi, "+ user_info.get("fullname")+"!");
+
+        String bid = "button_12";
+        int resID = getResources().getIdentifier(bid,"id",getPackageName());
+        ImageButton colorButton = (ImageButton)findViewById(resID);
+
+
+
+
+
+
+
+
 
     }
 
