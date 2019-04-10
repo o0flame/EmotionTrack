2b0529b7cc2eab507ce40cc5852a273d1da7d80b
RuxuZhang <zhangruxu413@gmail.com>
2019-04-09 21:17:57 -0400

    add pages

---
+++ app/src/main/java/com/example/emotiontrack/BuzzTipsChoose.java
@@ -0,0 +1,13 @@
+package com.example.emotiontrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuzzTipsChoose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buzz_tips_choose);
+    }
+}
