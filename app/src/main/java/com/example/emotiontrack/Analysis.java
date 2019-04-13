695755abbd14fd1de54b78054d563dadaa470592
RuxuZhang <zhangruxu413@gmail.com>
2019-04-13 16:08:47 -0400

    change intent

--- app/src/main/java/com/example/emotiontrack/Analysis.java
+++ app/src/main/java/com/example/emotiontrack/Analysis.java
@@ -6,15 +6,66 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class Analysis extends AppCompatActivity {
 
     ImageButton back;
+    ImageView emoji;
+    TextView date;
+    TextView tag1;
+    TextView tag2;
+    TextView tag3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis);
+
+        /* intent setting */
+        Intent intent = getIntent();
+        int[] info = (int[]) intent.getSerializableExtra("this_emotion");
+        int this_emotion = info[0];
+        int day = info[1];
+
+        /* emoji */
+        emoji = findViewById(R.id.this_emoji);
+        if(this_emotion == 1) emoji.setBackgroundResource(R.drawable.laugh);
+        else if(this_emotion == 2) emoji.setBackgroundResource((R.drawable.meh));
+        else if(this_emotion == 3) emoji.setBackgroundResource(R.drawable.sad);
+
+        /*  date */
+        date = findViewById(R.id.this_date);
+        if(day == 0) date.setText("Sunday 3/20");
+        else if(day == 1) date.setText("Monday 3/21");
+        else if(day == 2) date.setText("Tuesday 3/22");
+        else if(day == 3) date.setText("Wednesday 3/23");
+        else if(day == 4) date.setText("Thursday 3/24");
+        else if(day == 5) date.setText("Friday 3/25");
+        else if(day == 6) date.setText("Saturday 3/26");
+
+        /* tags */
+        tag1 = findViewById(R.id.tag_1);
+        tag2 = findViewById(R.id.tag_2);
+        tag3 = findViewById(R.id.tag_3);
+        if(this_emotion == 1){
+            tag1.setText("Love");
+            tag2.setText("Excitement");
+            tag3.setText("Happy");
+        }
+        else if(this_emotion == 2){
+            tag1.setText("Peace");
+            tag2.setText("Just So-so");
+            tag3.setText("Nothing");
+        }
+        else if(this_emotion == 3){
+            tag1.setText("Cry");
+            tag2.setText("Exhausted");
+            tag3.setText("Despair");
+        }
 
         /* back button */
         back = findViewById(R.id.back_button);
--- app/src/main/java/com/example/emotiontrack/DashBoard.java
+++ app/src/main/java/com/example/emotiontrack/DashBoard.java
@@ -108,21 +108,112 @@ public class DashBoard extends AppCompatActivity {
             welcomeText = findViewById(R.id.welcome);
             welcomeText.setText("Hi, "+ user_info.get("fullname")+"!");
 
-            //database: last 7 days emotions including today. -->today, yesterday ....
-            //map last seven days to S-M-T...-Sat
-            for(int d=0;d<=6;d++){
-                int cur = day-d-1;
-                if(cur<0) cur+=7; // map 0-6 for sunday to sat
-                String bid = "rec_button_"+String.valueOf(cur)+"_"+String.valueOf(6-emotions[d]);
-                int resID = getResources().getIdentifier(bid,"id",getPackageName());
-                ImageButton colorButton = (ImageButton)findViewById(resID);
-                //colorButton.getDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
-                colorButton.setBackgroundResource(R.drawable.dark_rec);
+            // Sunday button
+            String bid = "rec_button_"+String.valueOf(0)+"_"+String.valueOf(6-emotions[0]);
+            int resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton sundayButton = (ImageButton)findViewById(resID);
+            sundayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 0);
+            sundayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 0);
+                }
+            });
 
-                colorButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(DashBoard.this, Analysis.class));
+            // Monday button
+            bid = "rec_button_"+String.valueOf(1)+"_"+String.valueOf(6-emotions[1]);
+            resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton mondayButton = (ImageButton)findViewById(resID);
+            mondayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 1);
+            mondayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 1);
+                }
+            });
+
+            // Tuesday button
+            bid = "rec_button_"+String.valueOf(2)+"_"+String.valueOf(6-emotions[2]);
+            resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton tuesdayButton = (ImageButton)findViewById(resID);
+            tuesdayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 2);
+            tuesdayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 2);
+                }
+            });
+
+            // Wednesday button
+            bid = "rec_button_"+String.valueOf(3)+"_"+String.valueOf(6-emotions[3]);
+            resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton wednesdayButton = (ImageButton)findViewById(resID);
+            wednesdayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 3);
+            wednesdayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 3);
+                }
+            });
+
+            // Thursday button
+            bid = "rec_button_"+String.valueOf(4)+"_"+String.valueOf(6-emotions[4]);
+            resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton thursdayButton = (ImageButton)findViewById(resID);
+            thursdayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 4);
+            thursdayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 4);
+                }
+            });
+
+            // Friday button
+            bid = "rec_button_"+String.valueOf(5)+"_"+String.valueOf(6-emotions[5]);
+            resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton fridayButton = (ImageButton)findViewById(resID);
+            fridayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 5);
+            fridayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 5);
+                }
+            });
+
+            // Saturday button
+            bid = "rec_button_"+String.valueOf(6)+"_"+String.valueOf(6-emotions[6]);
+            resID = getResources().getIdentifier(bid,"id",getPackageName());
+            ImageButton saturdayButton = (ImageButton)findViewById(resID);
+            saturdayButton.setBackgroundResource(R.drawable.dark_rec);
+            setEmojiHere(emotions, 6);
+            saturdayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setIntentHere(emotions, 6);
+                }
+            });
+
+//            //database: last 7 days emotions including today. -->today, yesterday ....
+//            //map last seven days to S-M-T...-Sat
+//            for(int d=0;d<=6;d++){
+//                int cur = day-d-1;
+//                if(cur<0) cur+=7; // map 0-6 for sunday to sat
+//                String bid = "rec_button_"+String.valueOf(cur)+"_"+String.valueOf(6-emotions[d]);
+//                int resID = getResources().getIdentifier(bid,"id",getPackageName());
+//                ImageButton colorButton = (ImageButton)findViewById(resID);
+//                //colorButton.getDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+//                colorButton.setBackgroundResource(R.drawable.dark_rec);
+//
+//                colorButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        startActivity(new Intent(DashBoard.this, Analysis.class));
 //                        int message = -1;
 //                        if(emotions[d] == -1) message = 2;
 //                        else if(emotions[d] >= 4) message = 1;
@@ -131,20 +222,20 @@ public class DashBoard extends AppCompatActivity {
 //                        Intent intent2 = new Intent(DashBoard.this, Analysis.class);
 //                        intent2.putExtra("this_emotion", (Serializable)message);
 //                        startActivity(intent2);
-                        //startActivityForResult(new Intent(DashBoard.this, Analysis.class), TO_ANALYSIS);
-                    }
-                });
-
-                String image_id_str = "Emotion_" + String.valueOf(cur);
-                int image_id = getResources().getIdentifier(image_id_str,"id",getPackageName());
-                ImageView imageView = findViewById(image_id);
-                if(emotions[d]==-1) imageView.setImageResource(R.drawable.meh); // replace with ? later on
-                else if(emotions[d]>=4) imageView.setImageResource(R.drawable.laugh);
-                else if(emotions[d]==3) imageView.setImageResource(R.drawable.meh);
-                else if(emotions[d]<=2) imageView.setImageResource(R.drawable.sad);
-
-            }
-        }// dashboard set end
+//                        //startActivityForResult(new Intent(DashBoard.this, Analysis.class), TO_ANALYSIS);
+//                    }
+//                });
+//
+//                String image_id_str = "Emotion_" + String.valueOf(cur);
+//                int image_id = getResources().getIdentifier(image_id_str,"id",getPackageName());
+//                ImageView imageView = findViewById(image_id);
+//                if(emotions[d]==-1) imageView.setImageResource(R.drawable.meh); // replace with ? later on
+//                else if(emotions[d]>=4) imageView.setImageResource(R.drawable.laugh);
+//                else if(emotions[d]==3) imageView.setImageResource(R.drawable.meh);
+//                else if(emotions[d]<=2) imageView.setImageResource(R.drawable.sad);
+//
+//            }
+        }// if syntax end
 
 
         /*
@@ -154,6 +245,30 @@ public class DashBoard extends AppCompatActivity {
         //colorButton.getDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
         colorButton.setBackgroundColor(Color.parseColor("#666bff"));*/
 
+    }// onCreate end
+
+    public void setIntentHere(int[] emotions, int e){
+        int message = -1;
+        if(emotions[e] == -1) message = 2;
+        else if(emotions[e] >= 4) message = 1;
+        else if(emotions[e] == 3) message = 2;
+        else if(emotions[e] <= 2) message = 3;  // 1: happy, 2: meh, 3: sad
+        int[] info = new int[2];
+        info[0] = message;
+        info[1] = e;
+        Intent intent2 = new Intent(DashBoard.this, Analysis.class);
+        intent2.putExtra("this_emotion", (Serializable)info);
+        startActivity(intent2);
+    }
+
+    public  void setEmojiHere(int[] emotions, int e){
+        String image_id_str = "Emotion_" + String.valueOf(e);
+        int image_id = getResources().getIdentifier(image_id_str,"id",getPackageName());
+        ImageView imageView = findViewById(image_id);
+        if(emotions[e]==-1) imageView.setImageResource(R.drawable.meh); // replace with ? later on
+        else if(emotions[e]>=4) imageView.setImageResource(R.drawable.laugh);
+        else if(emotions[e]==3) imageView.setImageResource(R.drawable.meh);
+        else if(emotions[e]<=2) imageView.setImageResource(R.drawable.sad);
     }
 
 //    @Override
