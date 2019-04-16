f510cdd214e04d5dde3cdfc9962cee56c10bacd7
RuxuZhang <zhangruxu413@gmail.com>
2019-04-16 13:05:30 -0400

    update change status

--- app/src/main/java/com/example/emotiontrack/ChangeStatus.java
+++ app/src/main/java/com/example/emotiontrack/ChangeStatus.java
@@ -2,8 +2,10 @@ package com.example.emotiontrack;
 
 import android.content.Intent;
 import android.media.Image;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -50,10 +52,41 @@ public class ChangeStatus extends AppCompatActivity {
         changeEmojiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChangeStatus.this, ChangeStatusPopWindow.class));
+                Intent intent = new Intent(ChangeStatus.this, ChangeStatusPopWindow.class);
+                startActivityForResult(intent, 1);
+                //startActivity(new Intent(ChangeStatus.this, ChangeStatusPopWindow.class));
             }
         });
 
 
+    }// onCreate end
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == 1) {
+                changeEmojiButton.setBackgroundResource(R.drawable.e1);
+            }else if(resultCode == 2){
+                changeEmojiButton.setBackgroundResource(R.drawable.e2);
+            }else if(resultCode == 3){
+                changeEmojiButton.setBackgroundResource(R.drawable.e3);
+            }else if(resultCode == 4){
+                changeEmojiButton.setBackgroundResource(R.drawable.e4);
+            }else if(resultCode == 5){
+                changeEmojiButton.setBackgroundResource(R.drawable.e5);
+            }else if(resultCode == 6){
+                changeEmojiButton.setBackgroundResource(R.drawable.e6);
+            }else if(resultCode == 7){
+                changeEmojiButton.setBackgroundResource(R.drawable.e7);
+            }else if(resultCode == 8){
+                changeEmojiButton.setBackgroundResource(R.drawable.e8);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/emotiontrack/ChangeStatusPopWindow.java
+++ app/src/main/java/com/example/emotiontrack/ChangeStatusPopWindow.java
@@ -1,11 +1,16 @@
 package com.example.emotiontrack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 public class ChangeStatusPopWindow extends Activity {
+    Button e1, e2, e3, e4, e5,e6,e7,e8;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +20,81 @@ public class ChangeStatusPopWindow extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         getWindow().setLayout((int)(dm.widthPixels), (int)(dm.heightPixels*0.4));
+
+        // buttons setting
+        e1 = findViewById(R.id.emotion1);
+        e2 = findViewById(R.id.emotion2);
+        e3 = findViewById(R.id.emotion3);
+        e4 = findViewById(R.id.emotion4);
+        e5 = findViewById(R.id.emotion5);
+        e6 = findViewById(R.id.emotion6);
+        e7 = findViewById(R.id.emotion7);
+        e8 = findViewById(R.id.emotion8);
+
+        //click actions
+        e1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                setResult(1,new Intent());
+                finish();
+            }
+        });
+
+        e2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(2,new Intent());
+                finish();
+            }
+        });
+
+        e3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(3,new Intent());
+                finish();
+            }
+        });
+
+        e4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(4,new Intent());
+                finish();
+            }
+        });
+
+        e5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(5,new Intent());
+                finish();
+            }
+        });
+
+        e6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(6,new Intent());
+                finish();
+            }
+        });
+
+        e7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(7,new Intent());
+                finish();
+            }
+        });
+
+        e8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(8,new Intent());
+                finish();
+            }
+        });
 
     }
 }
